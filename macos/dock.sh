bfaaa19d8922a35eb9ab4b062c017a8dfe8537b1
Jonathan Forby <forby@Wolverine.local>
2018-01-12 21:26:44 -0500

    Dock

--- macos/dock.sh
+++ macos/dock.sh
@@ -1,11 +1,17 @@
 #!/bin/sh
 
 dockutil --no-restart --remove all
-dockutil --no-restart --add "/Applications/Google Chrome.app"
-dockutil --no-restart --add "/Applications/Mail.app"
-dockutil --no-restart --add "/Applications/Calendar.app"
-dockutil --no-restart --add "/Applications/Utilities/Terminal.app"
+dockutil --no-restart --add "/Applications/App Store.app"
 dockutil --no-restart --add "/Applications/System Preferences.app"
-dockutil --no-restart --add "/Applications/Spotify.app"
+dockutil --no-restart --add "/Applications/Google Chrome.app"
+
+# Divider
+defaults write com.apple.dock persistent-apps -array-add '{tile-data={}; tile-type="spacer-tile";}'
+
+# Downloads Folder
+dockutil --no-restart --add "~/Downloads"
+
+# Recent Apps
+defaults write com.apple.dock persistent-others -array-add '{"tile-data" = {"list-type" = 1;}; "tile-type" = "recents-tile";}'
 
 killall Dock
